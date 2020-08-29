167f3c17904180871bc9e00d8ef6db670efd37ad
AIica <49744599+aiica@users.noreply.github.com>
2020-08-29 22:33:05 +0300

    Added docstrings for 'visualize.py'

--- tools/processing/visualize.py
+++ tools/processing/visualize.py
@@ -7,13 +7,19 @@ from sklearn.metrics import classification_report
 
 
 class Visualize:
+    """Class for visualize training results"""
+
     def __init__(self, model: any, columns: Dict, true_labels: Dict, predicted_labels: Dict) -> None:
+        """"Init function"""
+
         self.__model = model
         self.__columns = columns
         self.__true_labels = true_labels
         self.__predicted_labels = predicted_labels
 
     def __view_feature_importance(self) -> str:
+        """Function for getting feature_importances"""
+
         imp = {col: imp for imp, col
                in zip(self.__model.feature_importances_, self.__columns)}
         assert len(imp) == len(self.__columns)
@@ -21,6 +27,8 @@ class Visualize:
                          for col, imp in sorted(imp.items(), key=(lambda x: -x[1])))
 
     def __view_classification_class_report(self) -> DataFrame:
+        """Function for getting classification_class_report"""
+
         classes = np.unique(self.__true_labels)
         res = ps.DataFrame({"y": self.__true_labels, "p": self.__predicted_labels}, index=None)
         table = ps.DataFrame(index=classes, columns=classes)
@@ -42,10 +50,14 @@ class Visualize:
     def view_classification_class_report(self):
         return self.__view_classification_class_report
 
-    def __view_classification_report_sklearn(self):
+    def __view_classification_report_sklearn(self) -> None:
+        """Function for getting classification_report of sklearn"""
+
         return classification_report(self.__true_labels, self.__predicted_labels)
 
-    def view_report(self):
+    def view_report(self) -> None:
+        """Get full report for training process"""
+
         print(50 * '-')
         print(self.view_feature_importance())
         print(self.view_classification_report_sklearn())
